743738ebc9847c32d1d7718c8236e1f104442eb7
André Castro Garcia <andrecastrogarcia@gmail.com>
2022-08-08 12:46:56 -0400

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/andregarcia/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.andregarcia.workshopmongo.repository;
+
+import com.andregarcia.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+}
