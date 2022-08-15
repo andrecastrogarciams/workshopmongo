58ed46caab44f5b60f33fd04f802700b12e5a82b
André Castro Garcia <andrecastrogarcia@gmail.com>
2022-08-15 09:54:07 -0400

    Obtendo um post por id

---
+++ src/main/java/com/andregarcia/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.andregarcia.workshopmongo.services;
+
+import com.andregarcia.workshopmongo.domain.Post;
+import com.andregarcia.workshopmongo.repository.PostRepository;
+import com.andregarcia.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+
+    public Post findById(String id){
+        Post post = repo.findById(id).orElse(null);
+        if (post == null){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return post;
+    }
+
+}
