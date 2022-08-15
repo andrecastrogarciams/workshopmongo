99a7e689593c88845319425743d0d5a38ac2d6f8
André Castro Garcia <andrecastrogarcia@gmail.com>
2022-08-15 10:11:43 -0400

    Acrescentando comentários aos posts

--- src/main/java/com/andregarcia/workshopmongo/config/Instantiation.java
+++ src/main/java/com/andregarcia/workshopmongo/config/Instantiation.java
@@ -3,12 +3,14 @@ package com.andregarcia.workshopmongo.config;
 import com.andregarcia.workshopmongo.domain.Post;
 import com.andregarcia.workshopmongo.domain.User;
 import com.andregarcia.workshopmongo.dto.AuthorDTO;
+import com.andregarcia.workshopmongo.dto.CommentDTO;
 import com.andregarcia.workshopmongo.repository.PostRepository;
 import com.andregarcia.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import javax.xml.stream.events.Comment;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -38,6 +40,12 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, date, "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
         Post post2 = new Post(null, date.plusDays(2), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!",date,new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!",date.minusDays(1),new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",date.plusDays(2),new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/andregarcia/workshopmongo/domain/Post.java
+++ src/main/java/com/andregarcia/workshopmongo/domain/Post.java
@@ -1,6 +1,7 @@
 package com.andregarcia.workshopmongo.domain;
 
 import com.andregarcia.workshopmongo.dto.AuthorDTO;
+import com.andregarcia.workshopmongo.dto.CommentDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Post implements Serializable {
     @Id
@@ -22,4 +24,13 @@ public class Post implements Serializable {
     private String body;
     private AuthorDTO author;
 
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, LocalDate date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 }
